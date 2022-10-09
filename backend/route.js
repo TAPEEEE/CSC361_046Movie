c9c2a54bb9e56c30c8b78b65c0f5807c1af309c9
TAPE <nattapon.tape@g.swu.ac.th>
2022-10-09 15:37:44 +0700

    09/10/2022

--- backend/route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-
-var router = require('./server')
-app.use('/login', router)
-
-app.listen(5005, () => {
-    console.log('Server started seccess')
-})
